24ea3badc09f46ac7095afb643848b53dd3fd2d0
Hayden <donnellyhd@outlook.com>
2024-02-18 15:21:43 -0500

    Generalized vector matmul and added relu function.

--- src/main.c
+++ src/main.c
@@ -3,16 +3,27 @@
 #define NUM_HIDDEN_LAYERS 3
 #define HIDDEN_DIM 3
 #define OUTPUT_DIM 10
+#define BATCH_SIZE 1
 
-static inline void hidden_layer_forward(float* params_this_layer, float* in, float* out)
-{
-    int offset = 0;
-    for(int x = 0; x < HIDDEN_DIM; x++)
+static inline void vec_mat_mul(
+    float* mat, float* vec_in, float* vec_out, 
+    const int in_dim, const int out_dim
+){
+    int mat_offset = 0;
+    for(int x = 0; x < in_dim; x++)
     {
-        for(int y = 0; y < HIDDEN_DIM; y ++)
+        for(int y = 0; y < out_dim; y ++)
         {
-            out[x] += params_this_layer[offset++] * in[y];
+            vec_out[x] += mat[mat_offset++] * vec_in[y];
         }
+    }
+}
+
+static inline void relu(float* vec_in, float* vec_out, const int dim)
+{
+    for(int i = 0; i < dim; i++)
+    {
+        vec_out[i] = (vec_in[i] > 0.0f) ? vec_in[i] : 0.0f;
     }
 }
 
@@ -22,12 +33,13 @@ int main()
     float params[9] = 
     {
         1.0f, 0.0f, 0.0f,
-        0.0f, 2.0f, 0.0f,
+        0.0f, -2.0f, 0.0f,
         0.0f, 0.0f, 1.0f
     };
     float in[3] = {1.0f, 2.0f, 3.0f};
     float out[3] = {0.0f};
-    hidden_layer_forward(params, in, out);
+    vec_mat_mul(params, in, out, HIDDEN_DIM, HIDDEN_DIM);
+    relu(out, out, HIDDEN_DIM);
     for(int i = 0; i < HIDDEN_DIM; i++)
     {
         printf("%f ", out[i]);
