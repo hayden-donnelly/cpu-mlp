4d37a8b4cf4272ae43f5ba1952c10af9d0de9129
hayden-donnelly <donnellyhd@outlook.com>
2024-10-30 17:04:31 -0400

    Setup input, output, and activation cuDNN descriptors.

--- src/mlp_cudnn.cu
+++ src/mlp_cudnn.cu
@@ -22,9 +22,32 @@ int main()
     printf("Initialized cuDNN\n");
 
     load_mnist();
+    printf("Loaded MNIST\n");
     print_image(train_image[2]);
 
     constexpr int input_dim = 784;
     constexpr int hidden_dim = 256;
     constexpr int output_dim = 10;
+    constexpr int batch_size = 32;
+
+    // Setup input, output, and activation descriptors.
+    cudnnTensorDescriptor_t input_desc;
+    cudnnTensorDescriptor_t hidden_desc;
+    cudnnTensorDescriptor_t output_desc;
+
+    CHECK_CUDNN(cudnnCreateTensorDescriptor(&input_desc));
+    CHECK_CUDNN(cudnnCreateTensorDescriptor(&hidden_desc));
+    CHECK_CUDNN(cudnnCreateTensorDescriptor(&output_desc));
+
+    CHECK_CUDNN(cudnnSetTensor4dDescriptor(
+        input_desc, CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT, batch_size, 1, 1, input_dim
+    ));
+    CHECK_CUDNN(cudnnSetTensor4dDescriptor(
+        hidden_desc, CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT, batch_size, 1, 1, hidden_size
+    ));
+    CHECK_CUDNN(cudnnSetTensor4dDescriptor(
+        output_desc, CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT, batch_size, 1, 1, output_size
+    ));
+    
+    CHECK_CUDNN(cudnnDestroy(cudnn));
 }
